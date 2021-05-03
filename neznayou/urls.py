9666074c4a536133894409b36641a7fed8933008
Islam <78363252+aibekovislam@users.noreply.github.com>
2021-05-03 19:09:36 +0300

    added styles

--- neznayou/urls.py
+++ neznayou/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="index")
+    path('', index, name="home")
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
